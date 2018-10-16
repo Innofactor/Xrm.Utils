1f208b83d97cee62bec77691da729cd75a9917e4
Alexey Shytikov <alexey.shytikov@cinteros.se>
2018-10-16 15:23:30 +0200

    Initial commit

---
+++ Common/Interfaces/IContainable.cs
@@ -0,0 +1,25 @@
+﻿namespace Innofactor.Xrm.DevUtils.Common.Interfaces
+{
+    /// <summary>
+    /// Core object that helps to keep all objects and methods needed for CRM development in
+    /// package easy to access and operate
+    /// </summary>
+    public interface IContainable
+    {
+        /// <summary>
+        /// Get instance of the <see cref="ILoggable"/> assosiated with current container
+        /// </summary>
+        ILoggable Logger
+        {
+            get;
+        }
+
+        /// <summary>
+        /// Gets instance of <see cref="IServicable"/> assosiated with current container
+        /// </summary>
+        IServicable Service
+        {
+            get;
+        }
+    }
+}
---
+++ Common/Interfaces/IPluginContainer.cs
@@ -0,0 +1,31 @@
+﻿namespace Innofactor.Xrm.DevUtils.Common.Interfaces
+{
+    using Microsoft.Xrm.Sdk;
+    using System;
+
+    /// <summary>
+    /// Plugin specific extensions to IContainable
+    /// </summary>
+    public interface IPluginContainer : IContainable
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// Gets instance of the <see cref="IPluginExecutionContext"/> assosiated with current container
+        /// </summary>
+        IPluginExecutionContext Context
+        {
+            get;
+        }
+
+        /// <summary>
+        /// Gets instance of initial <see cref="IServiceProvider"/> assosiated with current plugin instance.
+        /// </summary>
+        IServiceProvider Provider
+        {
+            get;
+        }
+
+        #endregion Public Properties
+    }
+}
