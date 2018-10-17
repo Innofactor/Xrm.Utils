e7228318269a5e1866162fcefaf4b9310551f330
Alexey Shytikov <alexey.shytikov@cinteros.se>
2018-10-17 11:57:01 +0200

    Adding Logger extensions

---
+++ Common/Extensions/LoggerExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace Innofactor.Xrm.DevUtils.Common.Extensions
+{
+    using System;
+    using Microsoft.Xrm.Sdk;
+
+    public static class LoggerExtensions
+    {
+        #region Private Methods
+
+        public static void Trace(this ITracingService logger, Exception ex)
+        {
+            logger.Trace(ex.ToString());
+            logger.Trace(ex.Message);
+            logger.Trace(ex.Source);
+            logger.Trace(ex.StackTrace);
+            logger.Trace("---------------------------------------------------------");
+        }
+
+        #endregion Private Methods
+    }
+}
