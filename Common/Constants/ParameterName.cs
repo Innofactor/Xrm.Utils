02bf9330490dcb5ad9effeef67c01f98b4234d2a
Alexey Shytikov <alexey.shytikov@cinteros.se>
2018-10-17 11:19:29 +0200

    Adding parameters constant

---
+++ Common/Constants/ParameterName.cs
@@ -0,0 +1,56 @@
+﻿namespace Innofactor.Xrm.DevUtils.Common.Constants
+{
+    /// <summary>
+    /// Common parameter names for InputParameters and OutputParameters in plugin steps
+    /// </summary>
+    public static class ParameterName
+    {
+        #region Public Fields
+
+        public const string Assignee = "Assignee";
+        public const string AsyncOperationId = "AsyncOperationId";
+        public const string BusinessEntity = "BusinessEntity";
+        public const string BusinessEntityCollection = "BusinessEntityCollection";
+        public const string CampaignActivityId = "CampaignActivityId";
+        public const string CampaignId = "CampaignId";
+        public const string ColumnSet = "ColumnSet";
+        public const string Context = "context";
+        public const string ContractId = "ContractId";
+        public const string EmailId = "emailid";
+        public const string EndpointId = "EndpointId";
+        public const string EntityId = "EntityId";
+        public const string EntityMoniker = "EntityMoniker";
+        public const string ExchangeRate = "ExchangeRate";
+        public const string FaxId = "FaxId";
+        public const string FetchXml = "FetchXml";
+        public const string Id = "id";
+        public const string IncidentResolution = "IncidentResolution";
+        public const string ListId = "ListId";
+        public const string OptionalParameters = "OptionalParameters";
+        public const string PostBusinessEntity = "PostBusinessEntity";
+        public const string PostImage = "postimage";
+        public const string PostMasterBusinessEntity = "PostMasterBusinessEntity";
+        public const string PreBusinessEntity = "PreBusinessEntity";
+        public const string PreImage = "preimage";
+        public const string PreMasterBusinessEntity = "PreMasterBusinessEntity";
+        public const string PreSubordinateBusinessEntity = "PreSubordinateBusinessEntity";
+        public const string Query = "Query";
+        public const string RelatedEntities = "RelatedEntities";
+        public const string Relationship = "Relationship";
+        public const string ReturnDynamicEntities = "ReturnDynamicEntities";
+        public const string RouteType = "RouteType";
+        public const string Settings = "Settings";
+        public const string State = "State";
+        public const string Status = "Status";
+        public const string SubordinateId = "subordinateid";
+        public const string Target = "Target";
+        public const string TeamId = "TeamId";
+        public const string TemplateId = "TemplateId";
+        public const string TriggerAttribute = "TriggerAttribute";
+        public const string UpdateContent = "UpdateContent";
+        public const string ValidationResult = "ValidationResult";
+        public const string WorkflowId = "WorkflowId";
+
+        #endregion Public Fields
+    }
+}
