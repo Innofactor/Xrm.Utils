8ad8c750d3f07489ae6fb65327c38cbb93e88826
Alexey Shytikov <alexey.shytikov@cinteros.se>
2018-10-18 13:47:47 +0200

    Adding `MessageProcessingMode`

---
+++ Common/Constants/MessageProcessingMode.cs
@@ -0,0 +1,8 @@
+﻿namespace Innofactor.Xrm.Utils.Common.Constants
+{
+    public static class MessageProcessingMode
+    {
+        public const int Asynchronous = 1;
+        public const int Synchronous = 0;
+    }
+}
