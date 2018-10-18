bcc78ffa376636657e88ab3987d4354fb01d9eef
Alexey Shytikov <alexey.shytikov@cinteros.se>
2018-10-18 08:56:34 +0200

    Adding main files

---
+++ Common/PluginBase.cs
@@ -0,0 +1,60 @@
+﻿namespace Innofactor.Xrm.DevUtils.Common
+{
+    using System;
+    using System.Diagnostics;
+    using Microsoft.Xrm.Sdk;
+
+    /// <summary>
+    /// MS CRM Base Plugin. Makes usage of <see cref="PluginContainer"/> more easy.
+    /// </summary>
+    public abstract class PluginBase : IPlugin
+    {
+        #region Private Fields
+
+        private readonly string name;
+
+        #endregion Private Fields
+
+        #region Public Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PluginBase"/> class. Will use refrected name of the calling plugin.
+        /// </summary>
+        public PluginBase()
+            : base()
+        {
+            name = (new StackTrace()).GetFrame(1).GetMethod().ReflectedType.Name;
+        }
+
+        #endregion Public Constructors
+
+        #region Public Methods
+
+        /// <summary>
+        /// Default entry point for CRM plugin
+        /// </summary>
+        /// <param name="serviceProvider"></param>
+        public void Execute(IServiceProvider serviceProvider) => 
+            new PluginContainer(serviceProvider)
+            {
+                Validator = new Predicate<IPluginExecutionContext>(Validate),
+                Action = new Action<PluginContainer>(Execute)
+            }
+            .Execute();
+
+        /// <summary>
+        /// Main entry point for the plugin
+        /// </summary>
+        /// <param name="container"></param>
+        public abstract void Execute(PluginContainer container);
+
+        /// <summary>
+        /// Method to validate plugin's execution context
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public abstract bool Validate(IPluginExecutionContext context);
+
+        #endregion Public Methods
+    }
+}
