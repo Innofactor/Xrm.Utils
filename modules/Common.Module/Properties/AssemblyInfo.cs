fcdfe17d6d3074409e69de120f6b5665f0f35cf0
Alexey Shytikov <alexey.shytikov@cinteros.se>
2018-10-18 11:34:43 +0200

    Moving files between folders

--- modules/Common.Module/Properties/AssemblyInfo.cs
+++ modules/Common.Module/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Common.Slim")]
+[assembly: AssemblyTitle("Common.Module")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Common.Slim")]
+[assembly: AssemblyProduct("Common.Module")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("fdbf5769-343e-433c-956f-12f7bc87a9d1")]
+[assembly: Guid("dd7194a9-1074-425b-be3f-86999dbe6336")]
 
 // Version information for an assembly consists of the following four values:
 //
