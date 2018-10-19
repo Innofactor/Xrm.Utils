15452d1fa4364cbce3619b1a88965a3cc4f4bcc8
Alexey Shytikov <alexey.shytikov@cinteros.se>
2018-10-19 08:49:14 +0200

    Reorganizing files

---
+++ Innofactor.Xrm.Utils.Workflow/Interfaces/IActivityContainer.cs
@@ -0,0 +1,32 @@
+﻿namespace Innofactor.Xrm.Utils.Workflow.Interfaces
+{
+    using System.Activities;
+    using Cinteros.Crm.Utils.Common.Interfaces;
+    using Microsoft.Xrm.Sdk.Workflow;
+
+    /// <summary>
+    /// Plugin specific extensions to IContainable
+    /// </summary>
+    public interface IActivityContainer : IContainable
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// Gets instance of the <see cref="CodeActivityContext"/> assosiated with current container
+        /// </summary>
+        CodeActivityContext ActivityContext
+        {
+            get;
+        }
+
+        /// <summary>
+        /// Gets instance of the <see cref="IWorkflowContext"/> assosiated with current container
+        /// </summary>
+        IWorkflowContext WorkflowContext
+        {
+            get;
+        }
+
+        #endregion Public Properties
+    }
+}
