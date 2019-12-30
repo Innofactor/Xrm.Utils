ec2849b290e107af20d07673b8bd962547805ec1
Imran Akram <imran.akram@cinteros.se>
2019-12-30 10:56:19 +0100

    Adding Constants class

---
+++ Innofactor.Xrm.Utils.Common/Misc/Constants.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace Innofactor.Xrm.Utils.Common.Misc
+{
+    /// <summary>General constants used Xrm Utils code</summary>
+    public static class Constants
+    {
+        /// <summary>Entities that do not have attribute StateCode</summary>
+        public static readonly List<string> StatecodelessEntities = new List<string>(new string[] {
+            "activityparty",
+            "activitymimeattachment",
+            "annotation",
+            "annualfiscalcalendar",
+            "attachment",
+            "customeraddress",
+            "invoicedetail",
+            "listmember",
+            "notification",
+            "opportunityproduct",
+            "post",
+            "postcomment",
+            "postfollow",
+            "postlike",
+            "quotedetail",
+            "report",
+            "resource",
+            "role",
+            "salesorderdetail",
+            "site",
+            "subject",
+            "systemuser",
+            "team"
+        });
+    }
+}
